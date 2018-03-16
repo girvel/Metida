e756f414ee392e9af1a063312feea1de39392555
girvel <widauka@ya.ru>
2018-03-16 19:26:46 +0500

    Initial

---
+++ Metida.Controls/DiagramCanvas.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Metida.Controls
+{
+    public class DiagramCanvas : Canvas
+    {
+        static DiagramCanvas()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DiagramCanvas), new FrameworkPropertyMetadata(typeof(DiagramCanvas)));
+        }
+    }
+}
---
+++ Metida.Core/Node.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Metida.Core
+{
+    public class Node
+    {
+        public string Name { get; set; }
+
+        public List<Node> Children { get; set; } = new List<Node>();
+
+
+
+        public Node(string name)
+        {
+            Name = name;
+        }
+    }
+}
